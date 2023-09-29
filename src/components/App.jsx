8b55a99c84a03feca0fdafe345a2ff46512507ec
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-09-29 22:25:26 +0530

    Added CssBaseLine from @mui/material to App.js

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <h1>Hello,World - Filmpire</h1>
+  </div>
+);
+
+export default App;
