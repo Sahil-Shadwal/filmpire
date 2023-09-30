9e6c5ae27e26dcb9286b4743e0d4890d31c606e7
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-09-30 15:09:21 +0530

    Added navbar but there is some issue with mobile view

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 // import { ReactDOM } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+const theme = createTheme({});
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
