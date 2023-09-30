5b059478632a0d0b7bbd580a1b1d45a01623ecdb
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-09-30 09:47:51 +0530

    Added routing system for FILMPIRE using react-router-dom and structurized componenets in component folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+// import Actors from './Actors/actors';
+// import MovieInformation from './MovieInformation/MovieInformation';
+// import Movies from './Movies/Movies';
+// import NavBar from './NavBar/NavBar';
+// import Profile from './Profile/Profile';
 
 const App = () => (
   <div>
     <CssBaseline />
-    <h1>Hello,World - Filmpire</h1>
+    {/* <NavBar /> */}
+    <main>
+      <Routes>
+        <Route path="/" element={<MovieInformation />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/actors" element={<Actors />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </main>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import { ReactDOM } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
