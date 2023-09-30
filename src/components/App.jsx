540163213ec29ebfb5a4d598f63dfac890f9b4e5
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-09-30 10:52:33 +0530

    Using @mui/styles for styling but hit some obstacle  while making it work with react 18

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
-import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
-// import Actors from './Actors/actors';
-// import MovieInformation from './MovieInformation/MovieInformation';
-// import Movies from './Movies/Movies';
-// import NavBar from './NavBar/NavBar';
-// import Profile from './Profile/Profile';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    {/* <NavBar /> */}
-    <main>
-      <Routes>
-        <Route path="/" element={<MovieInformation />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/actors" element={<Actors />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </main>
-  </div>
-);
+import useStyles from './styles';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element={<MovieInformation />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
