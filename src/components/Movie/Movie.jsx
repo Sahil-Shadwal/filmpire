47f265f8b0aad6309193034945580f38b472d275
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-01 19:18:48 +0530

    Created poster view for movies

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -6,10 +6,23 @@ import useStyles from './styles';
 
 const Movie = ({ movie, i }) => {
   const classes = useStyles();
-  console.log(movie, i);
   return (
-    <Grid item xs={12} sm={6} md={4} lg={4} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant="h5">{movie.title}</Typography>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            alt={movie.title}
+            className={classes.image}
+            src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.movienewz.com/img/films/poster-holder.jpg'}
+          />
+          <Typography className={classes.title} variant="h5">{movie.title}</Typography>
+          <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
     </Grid>
   );
 };
--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,14 +4,35 @@ export default makeStyles((theme) => ({
   movie: {
     padding: '10px',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
   title: {
-    coloe: theme.palette.text.primary,
+    color: theme.palette.text.primary,
     textOverflow: 'ellipsis',
     width: '230px',
     whiteSpace: 'nowrap',
     overflow: 'hidden',
     marginTop: '10px',
-    marginBottom: '0',
+    marginBottom: 0,
     textAlign: 'center',
+    textDecoration: 'none',
   },
 }));
