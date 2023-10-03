fbb637bc04097f51fbd3a094a9ed02a442a85cf5
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-03 10:09:08 +0530

    Added logout button in profile.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 import { userSelector } from '../../features/auth';
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
 
-  console.log(user);
+  const favouriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
 
   return (
-    <div>Profile - {user.username} </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favouriteMovies.length
+        ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
+        : (
+          <Box>
+            FAVORITE MOVIES
+          </Box>
+        ) }
+    </Box>
   );
 };
 
