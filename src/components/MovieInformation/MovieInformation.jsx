6cea937bdecc65a677665b4487fb74b5ca3a2090
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-03 14:44:15 +0530

    Created movie information page , added poster , tagline , rating , language and many more. And on the top of that made it resposive for mobile and tablets too.

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -5,12 +5,76 @@ import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import useStyles from './styles';
+import { useGetMovieQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
+
 const MovieInformation = () => {
-  console.log('Movie Information');
   const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery({ id });
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back.</Link>
+      </Box>
+    );
+  }
 
   return (
-    <div>MovieInformation {id} </div>
+    <Grid container className={classes.containerSpaceAround}>
+      <Grid item xs={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/original${data.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography varient="subtitle1" gutterBottom style={{ marginLeft: '10px' }}>
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime}min {data?.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}
+          </Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link
+              key={genre.name}
+              className={classes.links}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+            >
+              <img src={genreIcons[genre.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color="textPrimary" varient="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
