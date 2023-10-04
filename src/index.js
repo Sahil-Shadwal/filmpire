7eae607bd979df4699f9e96a7d2ab71b60d26351
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-04 18:57:03 +0530

    Added dark mode

--- src/index.js
+++ src/index.js
@@ -2,23 +2,22 @@ import React from 'react';
 // import { ReactDOM } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 
 import App from './components/App.jsx';
 import store from './app/store.js';
 import './index.css';
+import ToggleColorMode from './utils/ToggleColorMode.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-const theme = createTheme({});
 
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <ToggleColorMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </ToggleColorMode>
   </Provider>,
 );
