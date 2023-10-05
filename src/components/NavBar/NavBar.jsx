561b1f04a4187a7ffcd6be270d4d78ac7a9bb57c
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-05 12:21:56 +0530

    Fixed some minor issues

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -76,6 +76,7 @@ const NavBar = () => {
                 <Avatar
                   style={{ width: 30, height: 30 }}
                   alt="Profile"
+                  src={`https://www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
                 />
               </Button>
             )}
